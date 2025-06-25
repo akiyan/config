5a637ce4e731dc8f65a0abb52968db58b8d3a80f
akiyan <akiyan.com@gmail.com>
2025-06-25 15:03:01 +0900

    nvim

--- .config/nvim-lua/init.lua
+++ .config/nvim-lua/init.lua
@@ -50,10 +50,18 @@ vim.o.list = true
 vim.o.scrolloff = 3
 vim.opt.listchars = {tab='»-', trail='-', eol='↲', extends='»', precedes='«', nbsp='%'}
 vim.opt.whichwrap = "b,s,h,l,<,>,[,]"
+vim.g.mapleader = ","
+vim.g.maplocalleader = "\\"
 
 -- remap
 vim.keymap.set("n", "tn", ":tabn<CR>", { remap = true, silent = true })
 vim.keymap.set("n", "tp", ":tabp<CR>", { remap = true, silent = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { remap = true, silent = true })
+vim.keymap.set("n", "<C-h>", "<C-w>h", { remap = true, silent = true })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { remap = true, silent = true })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { remap = true, silent = true })
+vim.keymap.set("n", "<leader>", ":ClaudeCode<CR>", { remap = true, silent = true })
+
 vim.keymap.set("", "x", '"_x')
 
 vim.cmd[[colorscheme desert]]
@@ -87,13 +95,41 @@ require("lazy").setup({
   },
   {'tpope/vim-surround'},
   {'akiyan/vim-textobj-xml-attribute', dependencies = {'kana/vim-textobj-user'}},
+  -- バッファ側に移動できない...
+  -- {
+  --   "coder/claudecode.nvim",
+  --   dependencies = { "folke/snacks.nvim" },
+  --   config = true,
+  --   keys = {
+  --     { "<leader>a", nil, desc = "AI/Claude Code" },
+  --     { "<leader>ac", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" },
+  --     { "<leader>af", "<cmd>ClaudeCodeFocus<cr>", desc = "Focus Claude" },
+  --     { "<leader>ar", "<cmd>ClaudeCode --resume<cr>", desc = "Resume Claude" },
+  --     { "<leader>aC", "<cmd>ClaudeCode --continue<cr>", desc = "Continue Claude" },
+  --     { "<leader>ab", "<cmd>ClaudeCodeAdd %<cr>", desc = "Add current buffer" },
+  --     { "<leader>as", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send to Claude" },
+  --     {
+  --       "<leader>as",
+  --       "<cmd>ClaudeCodeTreeAdd<cr>",
+  --       desc = "Add file",
+  --       ft = { "NvimTree", "neo-tree", "oil" },
+  --     },
+  --     -- Diff management
+  --     { "<leader>aa", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
+  --     { "<leader>ad", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
+  --   },
+  -- },
   {
     "greggh/claude-code.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim", -- Required for git operations
     },
     config = function()
-      require("claude-code").setup()
+      require("claude-code").setup({
+        window = {
+          position = "vertical",
+        },
+      })
     end
   },
   {
